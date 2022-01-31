e74cec75026cc63a0726d3a8519a1c8cbbe8b34b
Antoni Danov <antoni.danov@gmail.com>
2022-01-31 14:59:52 +0100

    Populate login component

--- EasyRepair/src/app/components/login/login.component.ts
+++ EasyRepair/src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ProfessionalService } from 'src/app/services/professionals/professional.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { RegisterValidators } from 'src/app/shared/utils';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: any;
+  error!: string;
+  role: boolean = false;
+  profession: boolean = false;
 
-  constructor() { }
+  constructor(private service: AuthService,
+    private userService: UserService,
+    private profService: ProfessionalService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = new FormGroup({
+      role: new FormControl(''),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[A-za-z0-9._%+-]+@[a-z]{3,6}\.[a-z]{2,4}$'),
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        RegisterValidators.WhiteSpacesNotAllowed
+      ]))
+    });
   }
 
+  raidoValue(data: any) {
+    if (data) {
+      this.role = true;
+
+      if (data.target.id === 'professional') {
+        this.profession = true;
+      } else {
+        this.profession = false;
+      }
+    }
+  }
+
+  SignInEmailAndPassword(userdata: any) {
+
+    if (userdata.role === 'user') {
+      this.service.UserSignIn(userdata).catch((err) => {
+        this.error = err.message;
+        this.form.reset();
+      });
+    } else if (userdata.role === 'professional') {
+      this.service.ProfessionalSignIn(userdata).catch((err) => {
+        this.error = err.message;
+        this.form.reset();
+      });
+    }
+  }
 }
