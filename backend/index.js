b47bb6be4058d9d459e4f921d101f06ed35cd33b
antoni-danov <antoni.danov@gmail.com>
2022-02-07 20:37:38 +0100

    Correction on backend express JS

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
+PORT = 3000 || process.env.PORT
 require('dotenv').config();
 const expressConfig = require('./config/express');
 const databaseConfig = require('./config/database');
 const routerConfig = require('./config/routes');
 
 start();
-async function start(){
+async function start() {
     const app = express();
 
     databaseConfig(app);
     expressConfig(app);
     routerConfig(app);
-    
-    app.listen(process.env.PORT, ()=>{
-        console.log(`The server is listening on port ${process.env.PORT}`);
+
+    app.listen(PORT, () => {
+        console.log(`The server is listening on port ${PORT}`);
     });
 }
