9ea6eed1c681a4ee2e904f4dead9e51a1506805e
antoni-danov <antoni.danov@gmail.com>
2022-02-10 12:10:40 +0100

    Change the port

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-PORT = 3000 || process.env.PORT
+PORT = process.env.PORT || 3000;
 require('dotenv').config();
 const expressConfig = require('./config/express');
 const databaseConfig = require('./config/database');
